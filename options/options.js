b98d987ff451d44b60523b3079d1290156c96c79
Crowdbotics <team@crowdbotics.com>
2022-05-11 10:09:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "riasat_foods_35167",
+  url: "https://riasat_foods_35167.botics.co",
+  api: "https://riasat_foods_35167.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
